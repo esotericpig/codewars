1839d8be1582d5152cdcb872e3276a5853a3361a
esotericpig <esotericpig@users.noreply.github.com>
2018-07-12 03:18:40 +0900

    Initial commit

---
+++ ruby/double_cola.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+###
+# I liked this Kata because it used The Big Bang Theory characters.
+# 
+# The best solution used a similar approach but divided by 2 down, which is far
+#   less code and more clever.
+# 
+# @author Jonathan Bradley Whited (@esotericpig)
+# @see    https://www.codewars.com/kata/double-cola/ruby
+# @rank   5 kyu
+###
+def whoIsNext(names,r)
+  clones = 1 # Number of clones (doubles) at this shift
+  shifts = 0 # Number of shifts done
+  
+  while (s = shifts + (names.length * clones)) < r
+    shifts = s
+    clones *= 2
+  end
+  
+  result = nil
+  
+  # Go to r (shift) from current shifts
+  names.each do |name|
+    result = name
+    break if (shifts += clones) >= r
+  end
+  result
+end
+
+names = ['Sheldon','Leonard','Penny','Rajesh','Howard']
+
+puts whoIsNext(names,1802) # Penny
+puts whoIsNext(names,50)   # Leonard
+
+puts unless ARGV.empty?
+ARGV.each do |arg|
+  puts "#{arg} => #{whoIsNext(names,arg.to_i)}"
+end
