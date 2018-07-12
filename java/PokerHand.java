104256ef5cebf5a1bc0ae70c9e8e3fbaa41f9395
esotericpig <esotericpig@users.noreply.github.com>
2018-07-13 01:17:34 +0900

    Fix for bicycle (wheel) straight: A 2 3 4 5

--- java/PokerHand.java
+++ java/PokerHand.java
@@ -18,6 +18,7 @@ import java.util.Arrays;
  * 
  * @author Jonathan Bradley Whited (@esotericpig)
  * @see    https://www.codewars.com/kata/ranking-poker-hands/java
+ * @see    https://en.wikipedia.org/wiki/Texas_hold_%27em#Hand_values
  * @rank   4 kyu
  */
 public class PokerHand {
@@ -37,13 +38,24 @@ public class PokerHand {
     test("Flush wins of straight",            win, "2H 3H 5H 6H 7H","2S 3H 4H 5S 6C");
     test("Equal straight is tie",             tie, "2S 3H 4H 5S 6C","3D 4C 5H 6H 2S");
     test("Straight wins of three of a kind",  win, "2S 3H 4H 5S 6C","AH AC 5H 6H AS");
-    test("3 Of a kind wins of two pair",      loss,"2S 2H 4H 5S 4C","AH AC 5H 6H AS");
-    test("2 Pair wins of pair",               win, "2S 2H 4H 5S 4C","AH AC 5H 6H 7S");
+    test("3 of a kind wins of two pair",      loss,"2S 2H 4H 5S 4C","AH AC 5H 6H AS");
+    test("2 pair wins of pair",               win, "2S 2H 4H 5S 4C","AH AC 5H 6H 7S");
     test("Highest pair wins",                 loss,"6S AD 7H 4S AS","AH AC 5H 6H 7S");
     test("Pair wins of nothing",              loss,"2S AH 4H 5S KC","AH AC 5H 6H 7S");
     test("Highest card loses",                loss,"2S 3H 6H 7S 9C","7H 3C TH 6H 9S");
     test("Highest card wins",                 win, "4S 5H 6H TS AC","3S 5H 6H TS AC");
     test("Equal cards is tie",                tie, "2S AH 4H 5S 6C","AD 4C 5H 6H 2C");
+    test("Bicycle straight beats 3 of a kind",win, "AS 2H 3C 4D 5S","KS KH KD 6S 7S");
+    
+    if(args.length > 0) {
+      System.out.println();
+      
+      // Example: java PokerHand 'AS 2H 3C 4D 5S' 'KS KH KD 6S 7S'
+      for(String arg: args) {
+        PokerHand pokerHand = new PokerHand(arg);
+        System.out.println(arg + " => " + pokerHand.value + ":" + Arrays.toString(pokerHand.cardValues));
+      }
+    }
   }
   
   public static void test(String msg,Result result,String hand1,String hand2) {
@@ -60,14 +72,18 @@ public class PokerHand {
     System.out.println(sb);
   }
   
+  // It would be nice to break this method out into multiple methods, but then
+  //   all of the local variables would have to be passed as args or stored as
+  //   instance variables.
   public PokerHand(String hand) {
-    String[] cardStrs = hand.split(" ");
+    String[] cardStrs = hand.split("\\s+");
     
     int[] ofAKinds = new int[13];
     
     boolean isFlush = true;
     char prevSuit = 0;
     
+    // Parse values, of a kinds, and flush
     for(int i = 0; i < cardStrs.length; ++i) {
       String cardStr = cardStrs[i];
       char suit = cardStr.charAt(1);
@@ -91,6 +107,7 @@ public class PokerHand {
       cardValues[i] = value;
     }
     
+    // Of a kinds?
     boolean is2OfAKind = false,is2Pairs = false;
     boolean is3OfAKind = false,is4OfAKind = false;
     
@@ -105,15 +122,28 @@ public class PokerHand {
       }
     }
     
+    // Straight?
     Arrays.sort(cardValues);
     
     boolean isStraight = true;
-    int straightValue = (cardValues[0] == 14) ? 1 : cardValues[0]; // Ace can be 1
+    int cardLen = cardValues.length;
+    int straightValue = cardValues[0];
+    boolean isBicycle = straightValue == 2 && cardValues[cardLen - 1] == 14; // Ace (14) can be 1
     
-    for(int value: cardValues) {
-      if(value != straightValue++) { isStraight = false; break; }
+    if(isBicycle) { --cardLen; }
+    
+    for(int i = 1; i < cardLen; ++i) {
+      int value = cardValues[i];
+      if(value != ++straightValue) { isStraight = false; break; }
     }
     
+    if(isBicycle && isStraight) {
+      // Change Ace value to 1 for a bicycle (wheel) straight
+      cardValues[cardLen] = 1;
+      Arrays.sort(cardValues);
+    }
+    
+    // Calculate ultimate value
     value = cardValues[cardValues.length - 1]; // High card
     
     if(isStraight && isFlush) {
@@ -134,6 +164,7 @@ public class PokerHand {
     int diff = value - hand.value;
     
     if(diff == 0) {
+      // Instead of this, you might be able to initialize #value to the sum in the constructor?
       for(int i = cardValues.length - 1; i >= 0; --i) {
         if((diff = cardValues[i] - hand.cardValues[i]) != 0) { break; }
       }
