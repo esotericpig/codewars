2043f0a915f3e2a645d17949fe4b8c3a2671e39f
esotericpig <esotericpig@users.noreply.github.com>
2018-07-13 15:53:52 +0900

    Initial commit

---
+++ javascript/counting_duplicates.js
@@ -0,0 +1,39 @@
+/**
+ * @author Jonathan Bradley Whited (@esotericpig)
+ * @see    https://www.codewars.com/kata/counting-duplicates/javascript
+ * @rank   6 kyu
+ */
+function duplicateCount(text) {
+  var dups = {}; // Duplicates
+  
+  return text.split('').map(c => {
+    c = c.toLowerCase();
+    
+    if(c in dups) {
+      if(dups[c]) {
+        dups[c] = false;
+        return c;
+      }
+    }
+    else {
+      dups[c] = true;
+      return null;
+    }
+  }).join('').length;
+}
+
+console.log(duplicateCount(""));                 // 0
+console.log(duplicateCount("abcde"));            // 0 (no characters repeats more than once)
+console.log(duplicateCount("aabbcde"));          // 2 ('a' and 'b')
+console.log(duplicateCount("aabBcde"));          // 2 (should ignore case)
+console.log(duplicateCount("Indivisibility"));   // 1 ('i' occurs six times)
+console.log(duplicateCount("Indivisibilities")); // 2 (characters may not be adjacent)
+
+if(process.argv.length > 2) {
+  console.log();
+  
+  for(var i = 2; i < process.argv.length; ++i) {
+    var text = process.argv[i];
+    console.log(text + ' => ' + duplicateCount(text));
+  }
+}
---
+++ javascript/human_readable_time.js
@@ -0,0 +1,31 @@
+/**
+ * I have no idea why this is 5 kyu...
+ * 
+ * @author Jonathan Bradley Whited (@esotericpig)
+ * @see    https://www.codewars.com/kata/human-readable-time/javascript
+ * @rank   5 kyu
+ */
+function humanReadable(seconds) {
+  return ""  + formatNum(Math.floor(seconds / 60 / 60)) +
+         ":" + formatNum(Math.floor(seconds / 60) % 60) +
+         ":" + formatNum(seconds % 60);
+}
+
+function formatNum(num) {
+  return ((num < 10) ? '0' : '') + num;
+}
+
+console.log(humanReadable(0));      // '00:00:00'
+console.log(humanReadable(5));      // '00:00:05'
+console.log(humanReadable(60));     // '00:01:00'
+console.log(humanReadable(86399));  // '23:59:59'
+console.log(humanReadable(359999)); // '99:59:59'
+
+if(process.argv.length > 2) {
+  console.log();
+  
+  for(var i = 2; i < process.argv.length; ++i) {
+    var secs = process.argv[i];
+    console.log(secs + ' => ' + humanReadable(secs));
+  }
+}
